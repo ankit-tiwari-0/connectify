a87b1adf04abef1bf7c6621a9a4032e8d5e72056
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-04 21:57:05 +0530

    password hashing

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,24 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
 export const signup = (req , res)=>{
-    res.send("signup")
+    const {fullName, email, password} = req.body;
+    try {
+        if (password.length < 6){
+            return res.status(400).json({ message: "Password must be at least 6 characters"});
+        }
+        
+        const user = await User.findOne({email});
+
+        if (user) return res.status(400).json({message: "Email already exists"});
+        
+        const salt = await bcrypt.genSalt(10)
+        const hashpassword = await bcrypt.hash(password,salt);
+
+       
+    } catch (error) {
+        
+    }
 }
 export const login = (req , res)=>{
     res.send("login")
