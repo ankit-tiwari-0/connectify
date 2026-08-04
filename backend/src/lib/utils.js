33976769a505eced7bff9263db0a4f8d42c16045
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-04 22:18:29 +0530

    add utils.js for generate the token

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+
+export const generateToken = (userId, res) =>{
+  const tokenss = jwt.sign({userId}, process.env.JWT, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", tokenss, {
+    maxAge: 7*24*60*60*1000,
+    httponly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development"
+  });
+
+  return tokenss;
+}
