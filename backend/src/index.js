441869625b34b2cec17c5201ceacb74873a76902
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-05 12:53:19 +0530

    add this  protectRoute middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import authrouter from "./routes/auth.route.js";
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
+import cookie from "cookie-parser"
 
 
 dotenv.config()
@@ -10,6 +11,7 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cookie())
 app.use("/api/auth", authrouter)
 
 
