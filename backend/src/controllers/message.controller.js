219241682fe6261a74db3da7c589f18eb519b094
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-08 11:55:31 +0530

    message work

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import imagekit from "../lib/cloudinary.js";
-import { getReceiverSocketId } from "../lib/socket.js";
+import { getReceiverSocketId ,io} from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import Users from "../models/user.model.js";
 
