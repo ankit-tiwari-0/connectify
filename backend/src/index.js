053c4c5769b17953b5ca0a7cbed8af89a7f59ede
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-08 14:20:51 +0530

    Ready to Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,13 @@ import { connectDB } from "./lib/db.js";
 import cookie from "cookie-parser"
 import cors from "cors"
 import { app, server } from "./lib/socket.js";
+import path from "path"
 
 
 dotenv.config()
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -25,7 +28,15 @@ app.use("/api/auth", authrouter)
 app.use("/api/messages", messagerouter)
 
 
-server.listen(3000, () =>{
-    console.log("server is running on port 3000");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () =>{
+    console.log("server is running on port" + PORT);
     connectDB()
 })
